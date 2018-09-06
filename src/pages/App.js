8210cc8b5d159ac6ff0108cc32539f7267a924fb
何文林 <3292400833@qq.com>
2018-09-06 16:19:39 +0800

    list css

--- src/pages/App.js
+++ src/pages/App.js
@@ -22,6 +22,13 @@ class App extends Component {
                   <p>asdasdasdasd</p>
                 </div>
               </li>
+              <li className="listItem active">
+                <img src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1934952861,3453579486&fm=27&gp=0.jpg" />
+                <div className="nameBox">
+                  <h3>名字阿萨德</h3>
+                  <p>asdasdasdasd</p>
+                </div>
+              </li>
             </ul>
           </div>
           <div className="contentBox">
