57ed5120bb5045cff7d8c9d2e7fc69e44f59f46f
何文林 <3292400833@qq.com>
2018-09-06 16:17:27 +0800

    left section css

--- src/pages/App.js
+++ src/pages/App.js
@@ -13,10 +13,18 @@ class App extends Component {
       <div className="App">
         <Header />
         <section className="container">
-          <div className="connet">
-            左边联系人
+          <div className="connetBox">
+            <ul>
+              <li className="listItem">
+                <img src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1934952861,3453579486&fm=27&gp=0.jpg" />
+                <div className="nameBox">
+                  <h3>名字阿萨德</h3>
+                  <p>asdasdasdasd</p>
+                </div>
+              </li>
+            </ul>
           </div>
-          <div className="content">
+          <div className="contentBox">
             右边聊天内容
           </div>
         </section>
